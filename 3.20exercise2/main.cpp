575242fc6f8e55605439bc170bd69d99534a3d63
FLH0202 <FLH0202@163.com>
2020-03-20 19:10:24 +0800

    lal

--- 3.20exercise2/main.cpp
+++ 3.20exercise2/main.cpp
@@ -1,4 +1,5 @@
 #include <opencv.hpp>
+
 using namespace cv;
 using namespace std;
 
@@ -11,19 +12,19 @@ int main()
 		cv::Mat stats;
 		cv::Mat cenrtroids;
 		cv::Mat resultMat;
-		int nComp = cv::connectedComponentsWithStats(binaryMat,labels,stats,cenrtroids,8);
-		cout << "the number is" << nComp << endl;
+	    int nComp = connectedComponentsWithStats(binaryMat,labels,stats,cenrtroids,8,4);
+		//int labelNum = cenrtroids.rows-1;
+		cout << "the number is" << nComp-1 << endl;
 		for (int i = 1; i < nComp; i++)
 		{
 			Rect bndbox;
-			bndbox.x = cenrtroids.at<int>(i, 0);
-			bndbox.y = cenrtroids.at<int>(i, 1);
-			bndbox.width = cenrtroids.at<int>(i, 2);
-			bndbox.height = cenrtroids.at<int>(i, 3);
-			rectangle(binaryMat, bndbox, CV_RGB(255, 255, 255), 1, 8, 0);
+	    	bndbox.x = stats.at<int>(i, 0);
+	        bndbox.y = stats.at<int>(i, 1);
+			bndbox.width = stats.at<int>(i, 2);
+			bndbox.height = stats.at<int>(i, 3);
+			rectangle(binaryMat, bndbox, CV_RGB(255, 255, 255), 3, 8, 0);
 		}
-		imshow("src1", src);
+	    imshow("src1", src);
 		imshow("sec2", binaryMat);
-		//imshow("src3", resultMat);
 		waitKey(0);
 }
