44f4e1942b1bfc02fe13052b942153b09c0bbfb9
FLH0202 <FLH0202@163.com>
2020-03-20 20:23:19 +0800

    last

--- 3.20exercise2/main.cpp
+++ 3.20exercise2/main.cpp
@@ -5,9 +5,12 @@ using namespace std;
 
 int main()
 {
+	    //图像二值化
 		cv::Mat src = imread("E:\\PIC\\coin.png",0);
 		cv::Mat binaryMat;
 		threshold(src, binaryMat, 0, 255, THRESH_OTSU);
+
+		//标记连通域，计算个数
 		cv::Mat labels;
 		cv::Mat stats;
 		cv::Mat cenrtroids;
@@ -15,6 +18,8 @@ int main()
 	    int nComp = connectedComponentsWithStats(binaryMat,labels,stats,cenrtroids,8,4);
 		//int labelNum = cenrtroids.rows-1;
 		cout << "the number is" << nComp-1 << endl;
+
+		//绘制bounding box
 		for (int i = 1; i < nComp; i++)
 		{
 			Rect bndbox;
@@ -24,6 +29,7 @@ int main()
 			bndbox.height = stats.at<int>(i, 3);
 			rectangle(binaryMat, bndbox, CV_RGB(255, 255, 255), 3, 8, 0);
 		}
+
 	    imshow("src1", src);
 		imshow("src2", binaryMat);
 		waitKey(0);
